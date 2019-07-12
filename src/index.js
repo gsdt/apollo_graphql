d1d0eedeeb8fea9676668f3828675547ebbe3ace
Nguyen Anh Tuan <giaosudauto.omega@gmail.com>
2019-07-12 13:51:11 +0700

    remove old shit

--- src/index.js
+++ src/index.js
@@ -2,21 +2,6 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const {Todo} = require('./db')
-let todoDatabase = {
-  counter: 2,
-  data: [
-    {
-      id: 1,
-      content: 'Hack fb for me',
-      completed: true
-    },
-    {
-      id: 2,
-      content: 'Nothing better than me',
-      completed: false
-    }
-  ]
-}
 
 const server = new ApolloServer({ 
   typeDefs,
